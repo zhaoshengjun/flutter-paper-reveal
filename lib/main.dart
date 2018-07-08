1cda657696425d00cfb16b2ad93305338903d601
Sam <zhao.shengjun@gmail.com>
2018-07-08 18:15:00 +1000

    add padding

--- lib/main.dart
+++ lib/main.dart
@@ -36,19 +36,27 @@ class _MyHomePageState extends State<MyHomePage> {
           child: new Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                new Image.asset(
-                  'assets/hotels.png',
-                  width: 200.0,
-                  height: 200.0,
+                new Padding(
+                  padding: new EdgeInsets.only(bottom: 25.0),
+                  child: new Image.asset(
+                    'assets/hotels.png',
+                    width: 200.0,
+                    height: 200.0,
+                  ),
                 ),
-                new Text(
-                  'Hotels',
-                  style: new TextStyle(color: Colors.white, fontSize: 34.0),
-                ),
-                new Text(
-                  'This is a body text',
-                  textAlign: TextAlign.center,
-                  style: new TextStyle(color: Colors.white, fontSize: 18.0),
+                new Padding(
+                    padding: new EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    child: new Text(
+                      'Hotels',
+                      style: new TextStyle(color: Colors.white, fontSize: 34.0),
+                    )),
+                new Padding(
+                  padding: new EdgeInsets.only(bottom: 75.0),
+                  child: new Text(
+                    'This is a body text',
+                    textAlign: TextAlign.center,
+                    style: new TextStyle(color: Colors.white, fontSize: 18.0),
+                  ),
                 )
               ]),
         )
