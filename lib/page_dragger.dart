49647e9958253b022e4602c54ad0474e4c3acf2b
Sam <zhao.shengjun@gmail.com>
2018-07-11 23:35:43 +1000

    animated page dragger

--- lib/page_dragger.dart
+++ lib/page_dragger.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'pager_indicator.dart';
@@ -66,9 +67,64 @@ class _PageDraggerState extends State<PageDragger> {
   }
 }
 
+class AnimatedPageDragger {
+  static const PERCENT_PER_MILLISECOND = 0.005;
+  final slideDirection;
+  final transitionGoal;
+
+  AnimationController completionAnimationController;
+
+  AnimationPageDragger(
+      {this.slideDirection,
+      this.transitionGoal,
+      slidePercent,
+      StreamController<SlideUpdate> slideUpdateStream,
+      TickerProvider vsync}) {
+        final startSlidePercent = slidePercent;
+    var endSlidePercent;
+    var duration;
+    if (transitionGoal == TransitionGoal.open) {
+      endSlidePercent = 1.0;
+      final slideRemaining = 1.0 - slidePercent;
+      duration = new Duration(
+          milliseconds: (slidePercent / PERCENT_PER_MILLISECOND).round());
+    } else {
+      endSlidePercent = 0.0;
+      duration = new Duration(
+          milliseconds: (slidePercent / PERCENT_PER_MILLISECOND).round());
+    }
+
+    completionAnimationController =
+        new AnimationController(duration: duration, vsync: vsync)
+          ..addListener(() {
+            final slidePercent = lerpDouble(startSlidePercent, endSlidePercent,
+                completionAnimationController.value);
+            slideUpdateStream.add(new SlideUpdate(
+                UpdateType.animating, slideDirection, slidePercent));
+          })
+          ..addStatusListener((AnimationStatus status) {
+            if (status == AnimationStatus.completed) {
+              new SlideUpdate(UpdateType.doneAnimating, slideDirection, endSlidePercent)
+            }
+          });
+  }
+
+  run() {
+    completionAnimationController.forward(from:0.0);
+  }
+
+  dispose() {
+    completionAnimationController.dispose();
+  }
+}
+
+enum TransitionGoal { open, close }
+
 enum UpdateType {
   dragging,
   doneDragging,
+  animating,
+  doneAnimating,
 }
 
 class SlideUpdate {
